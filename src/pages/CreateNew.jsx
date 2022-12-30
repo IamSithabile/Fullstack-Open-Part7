d3ec56996ad2319098ac137cb4ed5857ea7aa5a9
IamSithabile <91020386+IamSithabile@users.noreply.github.com>
2022-12-30 10:58:22 +0200

    Exercise 7.4 anecdotes and hooks step 1

--- src/pages/CreateNew.jsx
+++ src/pages/CreateNew.jsx
@@ -1,16 +1,18 @@
 import { useState } from 'react'
 
+import { useField } from '../hooks'
+
 const CreateNew = props => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
 
   const handleSubmit = e => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0,
     })
   }
@@ -23,24 +25,27 @@ const CreateNew = props => {
           content
           <input
             name="content"
-            value={content}
-            onChange={e => setContent(e.target.value)}
+            type={content.type}
+            value={content.value}
+            onChange={content.onChange}
           />
         </div>
         <div>
           author
           <input
             name="author"
-            value={author}
-            onChange={e => setAuthor(e.target.value)}
+            type={author.type}
+            value={author.value}
+            onChange={author.onChange}
           />
         </div>
         <div>
           url for more info
           <input
             name="info"
-            value={info}
-            onChange={e => setInfo(e.target.value)}
+            type={info.type}
+            value={info.value}
+            onChange={info.onChange}
           />
         </div>
         <button>create</button>
